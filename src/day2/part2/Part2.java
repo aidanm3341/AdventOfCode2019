64dd1b2aaca9fdb961d3ec37ba7887ff444f4cdf
Aidan McPhelim <aidanmcp@hotmail.co.uk>
2021-11-27 21:03:19 +0000

    day 2 part 2

---
+++ src/day2/part2/Part2.java
@@ -0,0 +1,67 @@
+package day2.part2;
+
+import utils.InputReader;
+import utils.Pair;
+import utils.UnknownOpCodeException;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Part2 {
+    public Part2(){ }
+
+    public List<Integer> computeProgram(List<Integer> i){
+        int programCounter = 0;
+        int opCode;
+        do{
+            opCode = i.get(programCounter);
+            switch(opCode) {
+                case 1: // add
+                    int addition = i.get(i.get(programCounter + 1)) + i.get(i.get(programCounter + 2));
+                    i.set(i.get(programCounter + 3), addition);
+                    break;
+                case 2: // multiply
+                    int multiplication = i.get(i.get(programCounter + 1)) * i.get(i.get(programCounter + 2));
+                    i.set(i.get(programCounter + 3), multiplication);
+                    break;
+                case 99: // halt
+                    break;
+                default:
+                    throw new UnknownOpCodeException();
+            }
+            programCounter += 4;
+        } while (opCode != 99);
+        return i;
+    }
+
+    public Pair<Integer, Integer> getInputsWhichGive(int target) throws FileNotFoundException {
+        List<Integer> initialState = new InputReader().readProgram("day2.txt");
+        List<Integer> attemptState = new ArrayList<>();
+
+        int noun = 0, verb = 0;
+        for(noun = 0; noun <= 99; noun++) {
+            for(verb = 0; verb <= 99; verb++) {
+                attemptState.clear();
+                attemptState.addAll(initialState);
+                attemptState.set(1, noun);
+                attemptState.set(2, verb);
+                computeProgram(attemptState);
+                if(attemptState.get(0) == target) {
+                    return new Pair<>(noun, verb);
+                }
+            }
+        }
+        return new Pair<>(0, 0);
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        Part2 part1 = new Part2();
+
+        int target = 19690720;
+        Pair<Integer, Integer> answer = part1.getInputsWhichGive(target);
+
+        System.out.println(100 * answer.getFst() + answer.getSnd());
+    }
+}
