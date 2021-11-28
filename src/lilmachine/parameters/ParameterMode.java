e3381e06301e54a0535520b4a2190cfff4e1b369
Aidan McPhelim <aidanmcp@hotmail.co.uk>
2021-11-28 16:48:44 +0000

    refactored ParameterMode to work with chars not ints

--- src/lilmachine/parameters/ParameterMode.java
+++ src/lilmachine/parameters/ParameterMode.java
@@ -5,19 +5,10 @@ import lilmachine.exceptions.IllegalModeException;
 public enum ParameterMode {
     POSITION_MODE, IMMEDIATE_MODE;
 
-    static ParameterMode getMode(int num){
-        return switch (num){
-            case 0 -> POSITION_MODE;
-            case 1 -> IMMEDIATE_MODE;
-            default -> throw new IllegalModeException();
-        };
-    }
-
-    static ParameterMode getMode(char c){
-        int num = Integer.parseInt(String.valueOf(c));
-        return switch (num){
-            case 0 -> POSITION_MODE;
-            case 1 -> IMMEDIATE_MODE;
+    public static ParameterMode getMode(char c){
+        return switch (c){
+            case '0' -> POSITION_MODE;
+            case '1' -> IMMEDIATE_MODE;
             default -> throw new IllegalModeException();
         };
     }
